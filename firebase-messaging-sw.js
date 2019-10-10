1006a7f89ab18730bfa554dff5a1e8b4d6fbe0a2
vazquezagustin <agus@fidelitytools.com>
2019-10-10 09:15:18 -0300

    correciones

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,61 +1,46 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
-
-const pushConfig = {
-    apiKey: "AIzaSyC1TuClaax7dttB-eNVeaEEuN4j_yT6ojc",
-    authDomain: "fidelitypush.firebaseapp.com",
-    databaseURL: "https://fidelitypush.firebaseio.com",
-    projectId: "fidelitypush",
-    storageBucket: "fidelitypush.appspot.com",
-    messagingSenderId: "278414756419",
-    appId: "1:278414756419:web:28833783e5fd7919da22f6"
-};
-
-firebase.initializeApp(pushConfig);
-
-const messaging = firebase.messaging();
 
 
-self.addEventListener('notificationclick', function(event) {
-    if (!event.action) {
-        let url = 'https://www.google.com/';
 
-        event.waitUntil(
-            clients.matchAll({type: 'window'}).then( windowClients => {
+self.addEventListener('notificationclick', function (event) {
+  if (!event.action) {
+    let url = 'https://www.google.com/';
 
-                for (var i = 0; i < windowClients.length; i++) {
-                    var client = windowClients[i];
+    event.waitUntil(
+      clients.matchAll({ type: 'window' }).then(windowClients => {
 
-                    if (client.url === url && 'focus' in client) {
-                        return client.focus();
-                    }
-                }
+        for (var i = 0; i < windowClients.length; i++) {
+          var client = windowClients[i];
 
-                if (clients.openWindow) {
-                    return clients.openWindow(url);
-                }
-            })
-        );
-    }
-    console.log(event.action);
+          if (client.url === url && 'focus' in client) {
+            return client.focus();
+          }
+        }
 
-    switch (event.action) {
-      case 'coffee-action':
-        console.log('User ❤️️\'s coffee.');
-        break;
-      case 'doughnut-action':
-        console.log('User ❤️️\'s doughnuts.');
-        break;
-      case 'gramophone-action':
-        console.log('User ❤️️\'s music.');
-        break;
-      case 'atom-action':
-        console.log('User ❤️️\'s science.');
-        break;
-      default:
-        console.log(`Unknown action clicked: '${event.action}'`);
-        break;
-    }
+        if (clients.openWindow) {
+          return clients.openWindow(url);
+        }
+      })
+    );
+  }
+  console.log(event.action);
+
+  switch (event.action) {
+    case 'coffee-action':
+      console.log('User ❤️️\'s coffee.');
+      break;
+    case 'doughnut-action':
+      console.log('User ❤️️\'s doughnuts.');
+      break;
+    case 'gramophone-action':
+      console.log('User ❤️️\'s music.');
+      break;
+    case 'atom-action':
+      console.log('User ❤️️\'s science.');
+      break;
+    default:
+      console.log(`Unknown action clicked: '${event.action}'`);
+      break;
+  }
 });
 
 //     let url = 'https://www.google.com/';
@@ -86,7 +71,7 @@ self.addEventListener('notificationclick', function(event) {
 //       window.location.href = 'https://www.google.com/';
 //       return;
 //     }
-  
+
 //     switch (event.action) {
 //       case 'coffee-action':
 //         console.log('User ❤️️\'s coffee.');
@@ -106,79 +91,74 @@ self.addEventListener('notificationclick', function(event) {
 //     }
 // });
 
-self.addEventListener('push', function(event) {
-    let payload = JSON.parse(event.data.text());
 
-    const notificationTitle = payload.data.title;
-    const notificationOptions = {
-        body: payload.data.body,
-        badge: payload.data.click_action,
-        requireInteraction: true,
-        image: 'img/fav.fidelitytools.png',
-        badge: 'img/fav.fidelitytools.png',
-        icon: 'img/fav.fidelitytools.png',
-        actions: [
-            {
-              action: 'https://www.google.com/',
-              title: 'Coffee',
-              icon: 'img/fav.fidelitytools.png'
-            },
-            {
-              action: 'https://www.google.com/',
-              title: 'Doughnut',
-              icon: 'img/fav.fidelitytools.png'
-            },
-            {
-              action: 'gramophone-action',
-              title: 'gramophone',
-              icon: 'img/fav.fidelitytools.png'
-            },
-            {
-              action: 'atom-action',
-              title: 'Atom',
-              icon: 'img/fav.fidelitytools.png'
-            }
-          ]
-    };
 
-    return self.registration.showNotification(notificationTitle,
-        notificationOptions);
+
+
+//Section: Import
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+//Section: Vars
+var messaging;
+
+//Section: JS Default
+async function useFetch(url, method, headers, body) {
+  var response = {};
+
+  try {
+    await fetch(url,
+      {
+        method,
+        headers,
+        body
+      })
+      .then(async res => {
+        response.status = res.status;
+
+        await res.text().then(content => {
+          response.content = JSON.parse(content);
+        });
+      });
+
+    return response;
+  }
+  catch (ex) { console.log(ex); }
+}
+
+//Section Set Firebase 
+GetPushConfig().then(config => {
+  if (config) {
+    firebase.initializeApp(config);
+    messaging = firebase.messaging();
+  }
 });
 
+//Section: GotoAPI
+async function GetPushConfig() {
+  let response = await useFetch(`${xUrlApi}/api/Fidelitytools/ConfiguracionServerPush/Obtener`, 'GET', undefined, undefined);
 
-// messaging.setBackgroundMessageHandler(payload => {
-//     const notificationTitle = payload.data.title;
-//     const notificationOptions = {
-//         body: payload.data.body,
-//         badge: payload.data.click_action,
-//         requireInteraction: true,
-//         image: 'img/fav.fidelitytools.png',
-//         badge: 'img/fav.fidelitytools.png',
-//         icon: 'img/fav.fidelitytools.png',
-//         actions: [
-//             {
-//               action: 'coffee-action',
-//               title: 'Coffee',
-//               icon: 'img/fav.fidelitytools.png'
-//             },
-//             {
-//               action: 'doughnut-action',
-//               title: 'Doughnut',
-//               icon: 'img/fav.fidelitytools.png'
-//             },
-//             {
-//               action: 'gramophone-action',
-//               title: 'gramophone',
-//               icon: 'img/fav.fidelitytools.png'
-//             },
-//             {
-//               action: 'atom-action',
-//               title: 'Atom',
-//               icon: 'img/fav.fidelitytools.png'
-//             }
-//           ]
-//     };
+  if (response.status === 200) {
+    return response.content;
+  }
 
-//     return self.registration.showNotification(notificationTitle,
-//         notificationOptions);
-// });
+  return null;
+}
+
+//Section: Events
+self.addEventListener('push', function (event) {
+  let payload = JSON.parse(event.data.text()).data;
+
+  const notificationTitle = payload.title;
+  const notificationOptions = {
+    body: payload.body,
+    requireInteraction: payload.requireInteraction,
+    image: payload.image,
+    badge: payload.badge,
+    icon: payload.icon,
+    actions: payload.actions
+  };
+
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
